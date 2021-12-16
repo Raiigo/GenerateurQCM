d3fc2cd19953d363a2803b498b50c71fe6c891b1
Raiigo <nolann.juet@gmail.com>
2021-12-16 20:14:44 +0100

    Algo de changement de base

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Fonction donner la représentation d'un nombre decimal en une base quelconque
-# Le résultat est représenté comme une liste des coefficients devant les puisssances succecives de la base
+# Le résultat est représenté comme une liste des coefficients devant les puisssances successives de la base
 def decimal_to_base(number: int, base: int):
-    if base > 1: # Python ne permet pas de réstreindre le type des paramètres, les annotations servant uniquement à l'interpréteur et au linter, une vérification est donc necessaire
+    if base < 1: # Python ne permet pas de réstreindre le type des paramètres, les annotations servant uniquement à l'interpréteur et au linter, une vérification est donc necessaire
         print("La base se doit d'être un entier positif supérieur à 1")
     higher_power = 0
     for i in range(0, 10): # Determine la plus grande puissance de la base necessaire
@@ -9,6 +9,18 @@ def decimal_to_base(number: int, base: int):
             higher_power = i - 1
             break
     print(higher_power)
+    power_list = []
+    for i in range(0, higher_power + 1):
+        power_list.append(i)
+    power_list.reverse()
+    print(power_list)
+    coef_list = []
+    for i in power_list:
+        coef = round(number//pow(base, i))
+        number = number - coef * pow(base, i)
+        coef_list.append(coef)
+
+    print(coef_list)
 
 if __name__ == "__main__":
     decimal_to_base(10, 2)
